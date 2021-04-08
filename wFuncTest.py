c595ae7f4d0b45252096bebb373d5b30fef1aff6
BENJAMIN VERBEEK <BENJAMINVERBEEK79@gmail.com>
2021-04-08 13:48:48 +0200

    added F0-6 as lambda functions

--- wFuncTest.py
+++ wFuncTest.py
@@ -2,17 +2,23 @@ from math import sin, cos
 
 # defining F0-6 as in eq. (6.56) "Hardonic structure functions..."
 # Fäldt, Kupsc
-F0 = lambda th,th1,th2,ph1,ph2 : (sin(th))**2 * sin(th1) * sin(th2) * \
+
+# Note that these functions can be called with a list of args by using
+# F(*args) (* unpacks list/tuple)
+
+F0 = lambda th,th1,th2,ph1,ph2 : 1
+
+F1 = lambda th,th1,th2,ph1,ph2 : (sin(th))**2 * sin(th1) * sin(th2) * \
             cos(ph1) * cos(ph2) + (cos(th))**2 * cos(th1) * cos(th2)
 
-F1 = lambda th,th1,th2,ph1,ph2 : 1
+F2 = lambda th,th1,th2,ph1,ph2 : sin(th) * cos(th) * (sin(th1) * cos(th2) * \
+            cos(ph1) + cos(th1) * sin(th2) * cos(ph2))
 
-F2 = lambda th,th1,th2,ph1,ph2 : 1
+F3 = lambda th,th1,th2,ph1,ph2 : sin(th) * cos(th) * sin(th1) * sin(ph1)
 
-F3 = lambda th,th1,th2,ph1,ph2 : 1
+F4 = lambda th,th1,th2,ph1,ph2 : sin(th) * cos(th) * sin(th2) * sin(ph2)
 
-F4 = lambda th,th1,th2,ph1,ph2 : 1
+F5 = lambda th,th1,th2,ph1,ph2 : (cos(th))**2
 
-F5 = lambda th,th1,th2,ph1,ph2 : 1
-
-F6 = lambda th,th1,th2,ph1,ph2 : 1
+F6 = lambda th,th1,th2,ph1,ph2 : cos(th1) * cos(th2) - (sin(th))**2 * \
+            sin(th1) * sin(th2) * sin(ph1) * sin(ph2)
