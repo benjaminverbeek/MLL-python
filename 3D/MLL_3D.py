dc772fdbd49556692c03ffb7baa20d2a30e16aa5
Benjamin Verbeek <benjaminverbeek79@gmail.com>
2021-04-21 19:51:21 +0200

    Changed from iteration to map function, making for much faster execution. Still unclear if non MC is ok. This makes normalizing it instant.

--- 3D/MLL_3D.py
+++ 3D/MLL_3D.py
@@ -5,6 +5,8 @@ import os
 import time
 import numpy as np
 from scipy import optimize
+from scipy import integrate
+from matplotlib import pyplot as plt
 #from scipy.sparse import csc_matrix as csc
 
 ###### THEORY ######
@@ -15,9 +17,25 @@ F1 = lambda cos_th, cos_thP : math.sin(math.acos(cos_th)) * cos_th * cos_thP
 F2 = lambda cos_th, cos_thP : (cos_th)**2
 
 def WSingleTag(eta, delta_phi, xi):
+    '''Normalize this to get the PDF we want to optimize. Xi is the set of angles, packed as a list/tuple/...'''
     return F0(*xi) + eta * F2(*xi) + alpha * (1 - eta**2)**(0.5) * math.sin(delta_phi) * F1(*xi)  # W function
-#### END THEORY ####
 
+def WSingleTagIntegrable(eta, delta_phi):
+    '''Returns a function to integrate with defined parameters. Requiered format for scipy.integrate. \n
+    Returned function depends only on xi.'''
+
+    # xi is packed and then unpacked.
+    return (lambda *xi : F0(*xi) + eta * F2(*xi) + alpha * (1 - eta**2)**(0.5) * math.sin(delta_phi) * F1(*xi)  )
+
+def WSingleTagIntegrableMap(eta, delta_phi):
+    '''Returns a function to integrate with defined parameters. Requiered format for scipy.integrate. \n
+    Returned function depends only on xi.'''
+
+    # "xi is packed and then unpacked."   # CHANGED: only change to above is not packing first.
+    return (lambda xi : F0(*xi) + eta * F2(*xi) + alpha * (1 - eta**2)**(0.5) * math.sin(delta_phi) * F1(*xi)  )
+
+#### END THEORY ####
+    
 ###################
 # MC-integrator for normalization factors
 def MCintegral(par, uniformAngles):
@@ -26,7 +44,13 @@ def MCintegral(par, uniformAngles):
     for point in uniformAngles: # point is a 2D list
         s += WSingleTag(*par, point)
         n += 1
-    return 1/n * s * (2*math.pi)**2    # MC-integral
+    return 1/n * s * (2)**2    # MC-integral    # NOTE: the constant (2*math.pi)**2 doesn't matter for max? # PI^2 was off!
+
+def MCintegralImproved(par,uniformAngles):
+    func = WSingleTagIntegrableMap(par)
+    uAng = uniformAngles.copy()
+    res = list(map(func, uAng))
+    return 4*sum(res)/len(uniformAngles)    # area 2x2
 
 # Generalized LL-func.: send in a pdf too, and let par be n-dim, dataset var X be m-dim.
 def negLogLikelihood(par, var, pdf, normalizeSeparately=False, normalizationAngles=[]):
@@ -44,16 +68,32 @@ def negLogLikelihood(par, var, pdf, normalizeSeparately=False, normalizationAngl
 
     print("--------")
     if normalizeSeparately==True:
-        normalization = MCintegral(par, normalizationAngles)    # this one takes a really long time!
+        #normalization = MCintegral(par, normalizationAngles)    # this one takes a really long time!
+        normalization = integrate.nquad(WSingleTagIntegrable(*par), [[-1,1],[-1,1]])[0] # fast. returns (val, err) so must take [0]
+        # NOTE: Can't use non MC because we need to balance for detector?
+        print(normalization)
         t2 = time.time()
         print(f"One normalization done... took {t2 - t1:.2f} seconds.")
     else:
         normalization = 1
-
+    '''
     s = 0  # sum
     for v in var: # iterate over samples of xi
         # * unpacks the list of arguments
         s -= np.log(pdf(*par, v)/normalization) # log-sum of pdf gives LL. Negative so we minimize.
+    t3 = time.time()    # TODO: vectorized function calls?
+    '''
+    # attempt to use map instead of iterating...
+    print("ok0")
+    func = WSingleTagIntegrableMap(*par)
+    tempVar = var.copy()
+    print("ok1")
+    tempVar = list(map(func, tempVar))    # applies W       # takes time
+    print("ok2")
+    tempVar = list(map(np.log, tempVar))  # applies log     # takes time
+    print("ok3")
+    s = -1*sum(tempVar) + len(var)*np.log(normalization)   # sum, minus, normalize.
+    print("ok4")
     t3 = time.time()
     print(f"One set of parameters done. Took {t3 - t2:.2f} seconds.")    # takes a long time but not AS long as normalization.
     print(f"Total time for one run was {t3 - t1:.2f} seconds.")
@@ -67,18 +107,19 @@ def main():
 
     # Read angle data.
     xi_set = [ list(map(float,i.split())) for i in open("lAngles.txt").readlines() ]    # list (of lists)
-    xi_set = np.asarray(xi_set) # TODO: optimize.  Took a total of 835.8386192321777 seconds  without np.array (1mil norm.)
+    #xi_set = np.asarray(xi_set) # TODO: optimize.  Took a total of 835.8386192321777 seconds  without np.array (1mil norm.)
     print("Finished reading.")
     print(xi_set[0])
     print(f"Number of measurement points: {len(xi_set)}")
     print("--- %s seconds ---" % (time.time() - start_time))
     # takes approx. 1.3 seconds to read file as list. 3x as long to make it floats
     normalizationAngles = [ list(map(float,i.split())) for i in open("lPHSP_4Pi.txt").readlines() ]    # list (of lists)
-    normalizationAngles = np.asarray(normalizationAngles)
+    #normalizationAngles = np.asarray(normalizationAngles)
     # NOTE: cannot both be generators as it would create two open files? Or can they? Furthermore, going thru them
     # multiple times ruins it...
     print(normalizationAngles[0])
     print(f"Number of random angles for normalization: {len(normalizationAngles)}")
+    # NOTE: The normalization angles are not angles but rather cos(angles).
     print("--- %s seconds ---" % (time.time() - start_time))
 
     # Optimize:
@@ -92,14 +133,17 @@ def main():
     mLambda = 1.115683 # GeV, mass of lambda baryon (from PDG-live)
     tau = q**2/(4*mLambda**2)   # form factor
     tolerance = 10**-6
-    
+
+    # int2res = integrate.nquad(WSingleTagIntegrable(0.2, 42*math.pi/180), [[-1,1],[-1,1]])   # NOTE: WORKS
+    #print(f"Scipy integral: {int2res}")
+    #print(f"MC integral: {MCintegral([0.2, 42*math.pi/180], normalizationAngles)}")
     # eta = (tau - R^2)/(tau + R^2), 
     # where tau = q^2/(4m^2), q = 2.396 GeV here (momentum transfer), m = 1.115683 GeV (1115.683 MeV)
     # ===> eta = 0.217 (and delta_phi = 0.733 rad) is to be expected. 
 
     print("Optimizing...")
     # scipy existing minimizing function. 
-    res = optimize.minimize(negLogLikelihood, initial_guess, (xi_set[0:100000], WSingleTag, True, normalizationAngles[0:100000]), tol=tolerance, bounds=bnds)
+    res = optimize.minimize(negLogLikelihood, initial_guess, (xi_set[0:], WSingleTag, True, normalizationAngles[0:1]), tol=tolerance, bounds=bnds)
     # '''
     # PRESENT RESULTS:
     print(res)
